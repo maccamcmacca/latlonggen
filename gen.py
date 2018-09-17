3b6b265e6f5c12563396dd8c65e85b7aa9ebdf3c
maccamcmacca <ry1nmca@gmail.com>
2018-09-17 11:08:54 +0100

    initial commit

---
+++ gen.py
@@ -0,0 +1,16 @@
+import random
+import time
+
+#print("Enter latitude: ")
+latitude1 = input("Enter latitude: ")
+longitude1 = input("Enter longitude:")
+latitude2 = input("Enter latitude: ")
+longitude2 = input("Enter longitude:")
+
+random.seed(time.clock)
+print("Latitude")
+for x in range(0,51):
+	print(random.uniform(latitude1, latitude2))
+print("longitude")
+for x in range(0,51):
+	print(random.uniform(longitude1, longitude2))
