28cd5e6681a1d26d27e55bff121c02079664b57e
danieljmc <dmcguinness@dundee.ac.uk>
2018-09-20 21:02:49 +0100

    Added JSON input functionality, town variables

--- geogen.py
+++ geogen.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
+#Requires: input.json - {"town": "Dundee", "lat1": "56.478310", "lon1": "-3.050414", "lat2": "56.468327", "lon2": "-2.928002" }
 import random
 import time
-from geojson import LineString, Point, FeatureCollection, Feature
 import geojson
+import json
+from geojson import LineString, Point, FeatureCollection, Feature
+
+
+#Read Input json file
+f=open("input.json", "r")
+jstr = f.read()
+print(jstr)
+towns = json.loads(jstr)
 
 #Dundee Coordinates
 latitude1 = 56.478310
@@ -14,7 +23,6 @@ longitude2 = -2.928002
 #Misc Vars
 setLength = 5 #51
 maxIntensity = 100
-
 maxYear = 1918
 minYear = 1914
 maxMonth = 12
@@ -46,25 +54,33 @@ def generateISODate():
 
 
 feats = []
-for x in range(0, setLength):
 
-	#Generate Random Coordinates for features
-	rand_lat1 = random.uniform(latitude1, latitude2)
-	rand_lon1 = random.uniform(longitude1, longitude2)
-	rand_lat2 = random.uniform(latitude1, latitude2)
-	rand_lon2 = random.uniform(longitude1, longitude2)
+for y in range(0, len(towns)):
 
-	intensity = random.randint(0, maxIntensity)
-	curdate = generateISODate()
-	y = x*2		#use to set ID for both features
+	latitude1 = float(towns[y]["lat1"])
+	longitude1 = float(towns[y]["lon1"])
+	latitude2 = float(towns[y]["lat2"])
+	longitude2 = float(towns[y]["lon2"])
 
-	#Create LineString and Point features
-	line=Feature(geometry=LineString([(rand_lat1,rand_lon1),(rand_lat2,rand_lon2)]), id=(y), properties={"date": curdate})
-	point=Feature(geometry=Point((rand_lat2, rand_lon2)), id=(y+1), properties={"date": curdate, "intensity": intensity})
+	for x in range(0, setLength):
 
-	#Add new features to FeatureCollection
-	feats.append(line)
-	feats.append(point)
+		#Generate Random Coordinates for features
+		rand_lat1 = random.uniform(latitude1, latitude2)
+		rand_lon1 = random.uniform(longitude1, longitude2)
+		rand_lat2 = random.uniform(latitude1, latitude2)
+		rand_lon2 = random.uniform(longitude1, longitude2)
+
+		intensity = random.randint(0, maxIntensity)
+		curdate = generateISODate()
+		idvar = x*2		#use to set ID for both features
+
+		#Create LineString and Point features
+		line=Feature(geometry=LineString([(rand_lat1,rand_lon1),(rand_lat2,rand_lon2)]), id=(idvar), properties={"date": curdate})
+		point=Feature(geometry=Point((rand_lat2, rand_lon2)), id=(idvar+1), properties={"date": curdate, "intensity": intensity, "town": towns[y]["town"]})
+
+		#Add new features to FeatureCollection
+		feats.append(line)
+		feats.append(point)
 
 feat_col = FeatureCollection(feats)
 print("GeoJSON validity: " + str(feat_col.is_valid) + '\n')
